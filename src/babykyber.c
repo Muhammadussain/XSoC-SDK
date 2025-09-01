a8efd922aa8a0a026568e750e9170e39fef81b20
muhammadussain <pc21061.hussain@gmail.com>
2025-09-01 18:15:55 +0500

    Updated

--- src/babykyber.c
+++ src/babykyber.c
@@ -4,6 +4,11 @@
 
 // BabyKyber Constants and Values
 
+// Address constants for register mapping
+#define BABYKBER_BASE_ADDR     0x40007000
+#define BABYKBER_ADDR_INCREMENT 4
+#define BABYKBER_ADDR_OFFSETT 8
+
 // Random vector for encryption (8 values)
 const uint32_t r_vector[8] = {25, 26, 27, 28, 29, 30, 31, 32};
 
@@ -40,42 +45,42 @@ int main() {
     // Use the demo constants to set up the hardware registers
     
     // Set A_t matrix - using exact addresses from main.c
-    babykyber_write_reg(0x40007000, demo_A_t[0][0]);
-    babykyber_write_reg(0x40007004, demo_A_t[0][1]);
-    babykyber_write_reg(0x40007008, demo_A_t[0][2]);
-    babykyber_write_reg(0x40007012, demo_A_t[0][3]);
-    babykyber_write_reg(0x40007016, demo_A_t[1][0]);
-    babykyber_write_reg(0x40007020, demo_A_t[1][1]);
-    babykyber_write_reg(0x40007024, demo_A_t[1][2]);
-    babykyber_write_reg(0x40007028, demo_A_t[1][3]);
-    babykyber_write_reg(0x40007032, demo_A_t[2][0]);
-    babykyber_write_reg(0x40007036, demo_A_t[2][1]);
-    babykyber_write_reg(0x40007040, demo_A_t[2][2]);
-    babykyber_write_reg(0x40007044, demo_A_t[2][3]);
-    babykyber_write_reg(0x40007048, demo_A_t[3][0]);
-    babykyber_write_reg(0x40007052, demo_A_t[3][1]);
-    babykyber_write_reg(0x40007056, demo_A_t[3][2]);
-    babykyber_write_reg(0x40007060, demo_A_t[3][3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 0 * BABYKBER_ADDR_INCREMENT, demo_A_t[0][0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 1 * BABYKBER_ADDR_INCREMENT, demo_A_t[0][1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 2 * BABYKBER_ADDR_INCREMENT, demo_A_t[0][2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 2 * BABYKBER_ADDR_INCREMENT + BABYKBER_ADDR_OFFSETT, demo_A_t[0][3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 5 * BABYKBER_ADDR_INCREMENT + BABYKBER_ADDR_OFFSETT, demo_A_t[1][0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 8 * BABYKBER_ADDR_INCREMENT + BABYKBER_ADDR_OFFSETT, demo_A_t[1][1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 9 * BABYKBER_ADDR_INCREMENT + BABYKBER_ADDR_OFFSETT, demo_A_t[1][2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 10 * BABYKBER_ADDR_INCREMENT + BABYKBER_ADDR_OFFSETT, demo_A_t[1][3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 14 * BABYKBER_ADDR_INCREMENT + 2 * BABYKBER_ADDR_OFFSETT, demo_A_t[2][0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 15 * BABYKBER_ADDR_INCREMENT + 2 * BABYKBER_ADDR_OFFSETT, demo_A_t[2][1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 16 * BABYKBER_ADDR_INCREMENT + 3 * BABYKBER_ADDR_OFFSETT, demo_A_t[2][2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 17 * BABYKBER_ADDR_INCREMENT + 3 * BABYKBER_ADDR_OFFSETT, demo_A_t[2][3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 18 * BABYKBER_ADDR_INCREMENT + 3 * BABYKBER_ADDR_OFFSETT, demo_A_t[3][0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 19 * BABYKBER_ADDR_INCREMENT + 4 * BABYKBER_ADDR_OFFSETT, demo_A_t[3][1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 22 * BABYKBER_ADDR_INCREMENT + 4 * BABYKBER_ADDR_OFFSETT, demo_A_t[3][2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 24 * BABYKBER_ADDR_INCREMENT + 4 * BABYKBER_ADDR_OFFSETT, demo_A_t[3][3]);
     
     // Set s vector - using exact addresses from main.c
-    babykyber_write_reg(0x40007064, demo_s[0]);
-    babykyber_write_reg(0x40007068, demo_s[1]);
-    babykyber_write_reg(0x40007072, demo_s[2]);
-    babykyber_write_reg(0x40007076, demo_s[3]);
-    babykyber_write_reg(0x40007080, demo_s[4]);
-    babykyber_write_reg(0x40007084, demo_s[5]);
-    babykyber_write_reg(0x40007088, demo_s[6]);
-    babykyber_write_reg(0x40007092, demo_s[7]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 25 * BABYKBER_ADDR_INCREMENT + 4 * BABYKBER_ADDR_OFFSETT, demo_s[0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 26 * BABYKBER_ADDR_INCREMENT + 4 * BABYKBER_ADDR_OFFSETT, demo_s[1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 27 * BABYKBER_ADDR_INCREMENT + 5 * BABYKBER_ADDR_OFFSETT, demo_s[2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 31 * BABYKBER_ADDR_INCREMENT + 5 * BABYKBER_ADDR_OFFSETT, demo_s[3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 32 * BABYKBER_ADDR_INCREMENT + 5 * BABYKBER_ADDR_OFFSETT, demo_s[4]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 33 * BABYKBER_ADDR_INCREMENT + 5 * BABYKBER_ADDR_OFFSETT, demo_s[5]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 34 * BABYKBER_ADDR_INCREMENT + 5 * BABYKBER_ADDR_OFFSETT, demo_s[6]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 35 * BABYKBER_ADDR_INCREMENT + 6 * BABYKBER_ADDR_OFFSETT, demo_s[7]);
     
     // Set e vector - using exact addresses from main.c
-    babykyber_write_reg(0x40007096, demo_e[0]);
-    babykyber_write_reg(0x40007100, demo_e[1]);
-    babykyber_write_reg(0x40007104, demo_e[2]);
-    babykyber_write_reg(0x40007108, demo_e[3]);
-    babykyber_write_reg(0x40007112, demo_e[4]);
-    babykyber_write_reg(0x40007116, demo_e[5]);
-    babykyber_write_reg(0x40007120, demo_e[6]);
-    babykyber_write_reg(0x40007124, demo_e[7]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 39 * BABYKBER_ADDR_INCREMENT + 6 * BABYKBER_ADDR_OFFSETT, demo_e[0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 64 * BABYKBER_ADDR_INCREMENT + 6 * BABYKBER_ADDR_OFFSETT, demo_e[1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 65 * BABYKBER_ADDR_INCREMENT + 6 * BABYKBER_ADDR_OFFSETT, demo_e[2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 66 * BABYKBER_ADDR_INCREMENT + 6 * BABYKBER_ADDR_OFFSETT, demo_e[3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 67 * BABYKBER_ADDR_INCREMENT + 7 * BABYKBER_ADDR_OFFSETT, demo_e[4]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 71 * BABYKBER_ADDR_INCREMENT + 7 * BABYKBER_ADDR_OFFSETT, demo_e[5]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 80 * BABYKBER_ADDR_INCREMENT + 7 * BABYKBER_ADDR_OFFSETT, demo_e[6]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 81 * BABYKBER_ADDR_INCREMENT + 7 * BABYKBER_ADDR_OFFSETT, demo_e[7]);
     
     // Trigger key generation
     babykyber_write_reg(BABYKYBER_KEYGEN_TRIGGER, 0);
@@ -84,73 +89,73 @@ int main() {
     babykyber_write_reg(BABYKYBER_MESSAGE, demo_message);
     
     // Set public key for encryption - using exact addresses from main.c
-    babykyber_write_reg(0x40007132, demo_pk[0]);
-    babykyber_write_reg(0x40007136, demo_pk[1]);
-    babykyber_write_reg(0x40007140, demo_pk[2]);
-    babykyber_write_reg(0x40007144, demo_pk[3]);
-    babykyber_write_reg(0x40007148, demo_pk[4]);
-    babykyber_write_reg(0x40007152, demo_pk[5]);
-    babykyber_write_reg(0x40007156, demo_pk[6]);
-    babykyber_write_reg(0x40007160, demo_pk[7]);
-    babykyber_write_reg(0x40007164, demo_pk[8]);
-    babykyber_write_reg(0x40007168, demo_pk[9]);
-    babykyber_write_reg(0x40007172, demo_pk[10]);
-    babykyber_write_reg(0x40007176, demo_pk[11]);
-    babykyber_write_reg(0x40007180, demo_pk[12]);
-    babykyber_write_reg(0x40007184, demo_pk[13]);
-    babykyber_write_reg(0x40007188, demo_pk[14]);
-    babykyber_write_reg(0x40007192, demo_pk[15]);
-    babykyber_write_reg(0x40007196, demo_pk[16]);
-    babykyber_write_reg(0x40007200, demo_pk[17]);
-    babykyber_write_reg(0x40007204, demo_pk[18]);
-    babykyber_write_reg(0x40007208, demo_pk[19]);
-    babykyber_write_reg(0x40007212, demo_pk[20]);
-    babykyber_write_reg(0x40007216, demo_pk[21]);
-    babykyber_write_reg(0x40007220, demo_pk[22]);
-    babykyber_write_reg(0x40007224, demo_pk[23]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 76 * BABYKBER_ADDR_INCREMENT + 7 * BABYKBER_ADDR_OFFSETT, demo_pk[0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 77 * BABYKBER_ADDR_INCREMENT + 7 * BABYKBER_ADDR_OFFSETT, demo_pk[1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 80 * BABYKBER_ADDR_INCREMENT + 8 * BABYKBER_ADDR_OFFSETT, demo_pk[2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 81 * BABYKBER_ADDR_INCREMENT + 8 * BABYKBER_ADDR_OFFSETT, demo_pk[3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 82 * BABYKBER_ADDR_INCREMENT + 8 * BABYKBER_ADDR_OFFSETT, demo_pk[4]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 83 * BABYKBER_ADDR_INCREMENT + 9 * BABYKBER_ADDR_OFFSETT, demo_pk[5]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 87 * BABYKBER_ADDR_INCREMENT + 9 * BABYKBER_ADDR_OFFSETT, demo_pk[6]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 88 * BABYKBER_ADDR_INCREMENT + 9 * BABYKBER_ADDR_OFFSETT, demo_pk[7]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 89 * BABYKBER_ADDR_INCREMENT + 9 * BABYKBER_ADDR_OFFSETT, demo_pk[8]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 90 * BABYKBER_ADDR_INCREMENT + 9 * BABYKBER_ADDR_OFFSETT, demo_pk[9]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 91 * BABYKBER_ADDR_INCREMENT + 10 * BABYKBER_ADDR_OFFSETT, demo_pk[10]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 95 * BABYKBER_ADDR_INCREMENT + 10 * BABYKBER_ADDR_OFFSETT, demo_pk[11]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 96 * BABYKBER_ADDR_INCREMENT + 10 * BABYKBER_ADDR_OFFSETT, demo_pk[12]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 97 * BABYKBER_ADDR_INCREMENT + 10 * BABYKBER_ADDR_OFFSETT, demo_pk[13]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 98 * BABYKBER_ADDR_INCREMENT + 10 * BABYKBER_ADDR_OFFSETT, demo_pk[14]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 99 * BABYKBER_ADDR_INCREMENT + 11 * BABYKBER_ADDR_OFFSETT, demo_pk[15]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 103 * BABYKBER_ADDR_INCREMENT + 11 * BABYKBER_ADDR_OFFSETT, demo_pk[16]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 128 * BABYKBER_ADDR_INCREMENT + 11 * BABYKBER_ADDR_OFFSETT, demo_pk[17]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 129 * BABYKBER_ADDR_INCREMENT + 11 * BABYKBER_ADDR_OFFSETT, demo_pk[18]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 130 * BABYKBER_ADDR_INCREMENT + 11 * BABYKBER_ADDR_OFFSETT, demo_pk[19]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 131 * BABYKBER_ADDR_INCREMENT + 12 * BABYKBER_ADDR_OFFSETT, demo_pk[20]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 135 * BABYKBER_ADDR_INCREMENT + 12 * BABYKBER_ADDR_OFFSETT, demo_pk[21]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 144 * BABYKBER_ADDR_INCREMENT + 12 * BABYKBER_ADDR_OFFSETT, demo_pk[22]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 145 * BABYKBER_ADDR_INCREMENT + 12 * BABYKBER_ADDR_OFFSETT, demo_pk[23]);
     
     // Set r vector - using exact addresses from main.c
-    babykyber_write_reg(0x40007228, r_vector[0]);
-    babykyber_write_reg(0x40007232, r_vector[1]);
-    babykyber_write_reg(0x40007236, r_vector[2]);
-    babykyber_write_reg(0x40007240, r_vector[3]);
-    babykyber_write_reg(0x40007244, r_vector[4]);
-    babykyber_write_reg(0x40007248, r_vector[5]);
-    babykyber_write_reg(0x40007252, r_vector[6]);
-    babykyber_write_reg(0x40007256, r_vector[7]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 146 * BABYKBER_ADDR_INCREMENT + 12 * BABYKBER_ADDR_OFFSETT, r_vector[0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 148 * BABYKBER_ADDR_INCREMENT + 13 * BABYKBER_ADDR_OFFSETT, r_vector[1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 152 * BABYKBER_ADDR_INCREMENT + 13 * BABYKBER_ADDR_OFFSETT, r_vector[2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 160 * BABYKBER_ADDR_INCREMENT + 13 * BABYKBER_ADDR_OFFSETT, r_vector[3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 161 * BABYKBER_ADDR_INCREMENT + 13 * BABYKBER_ADDR_OFFSETT, r_vector[4]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 162 * BABYKBER_ADDR_INCREMENT + 13 * BABYKBER_ADDR_OFFSETT, r_vector[5]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 163 * BABYKBER_ADDR_INCREMENT + 14 * BABYKBER_ADDR_OFFSETT, r_vector[6]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 167 * BABYKBER_ADDR_INCREMENT + 14 * BABYKBER_ADDR_OFFSETT, r_vector[7]);
     
     // Set e1 vector - using exact addresses from main.c
-    babykyber_write_reg(0x40007260, e1_vector[0]);
-    babykyber_write_reg(0x40007264, e1_vector[1]);
-    babykyber_write_reg(0x40007268, e1_vector[2]);
-    babykyber_write_reg(0x40007272, e1_vector[3]);
-    babykyber_write_reg(0x40007276, e1_vector[4]);
-    babykyber_write_reg(0x40007280, e1_vector[5]);
-    babykyber_write_reg(0x40007284, e1_vector[6]);
-    babykyber_write_reg(0x40007288, e1_vector[7]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 168 * BABYKBER_ADDR_INCREMENT + 14 * BABYKBER_ADDR_OFFSETT, e1_vector[0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 169 * BABYKBER_ADDR_INCREMENT + 14 * BABYKBER_ADDR_OFFSETT, e1_vector[1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 170 * BABYKBER_ADDR_INCREMENT + 14 * BABYKBER_ADDR_OFFSETT, e1_vector[2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 171 * BABYKBER_ADDR_INCREMENT + 15 * BABYKBER_ADDR_OFFSETT, e1_vector[3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 175 * BABYKBER_ADDR_INCREMENT + 15 * BABYKBER_ADDR_OFFSETT, e1_vector[4]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 176 * BABYKBER_ADDR_INCREMENT + 15 * BABYKBER_ADDR_OFFSETT, e1_vector[5]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 177 * BABYKBER_ADDR_INCREMENT + 15 * BABYKBER_ADDR_OFFSETT, e1_vector[6]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 178 * BABYKBER_ADDR_INCREMENT + 15 * BABYKBER_ADDR_OFFSETT, e1_vector[7]);
     
     // Set e2 vector - using exact addresses from main.c
-    babykyber_write_reg(0x40007292, e2_vector[0]);
-    babykyber_write_reg(0x40007296, e2_vector[1]);
-    babykyber_write_reg(0x40007300, e2_vector[2]);
-    babykyber_write_reg(0x40007304, e2_vector[3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 179 * BABYKBER_ADDR_INCREMENT + 16 * BABYKBER_ADDR_OFFSETT, e2_vector[0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 183 * BABYKBER_ADDR_INCREMENT + 16 * BABYKBER_ADDR_OFFSETT, e2_vector[1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 192 * BABYKBER_ADDR_INCREMENT + 16 * BABYKBER_ADDR_OFFSETT, e2_vector[2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 193 * BABYKBER_ADDR_INCREMENT + 16 * BABYKBER_ADDR_OFFSETT, e2_vector[3]);
     
     // Trigger encryption
     babykyber_write_reg(BABYKYBER_ENCRYPT_TRIGGER, 0);
     
     // Set ciphertext for decryption - using exact addresses from main.c
-    babykyber_write_reg(0x40007308, demo_ct[0]);
-    babykyber_write_reg(0x40007312, demo_ct[1]);
-    babykyber_write_reg(0x40007316, demo_ct[2]);
-    babykyber_write_reg(0x40007320, demo_ct[3]);
-    babykyber_write_reg(0x40007324, demo_ct[4]);
-    babykyber_write_reg(0x40007328, demo_ct[5]);
-    babykyber_write_reg(0x40007332, demo_ct[6]);
-    babykyber_write_reg(0x40007336, demo_ct[7]);
-    babykyber_write_reg(0x40007340, demo_ct[8]);
-    babykyber_write_reg(0x40007344, demo_ct[9]);
-    babykyber_write_reg(0x40007348, demo_ct[10]);
-    babykyber_write_reg(0x40007352, demo_ct[11]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 194 * BABYKBER_ADDR_INCREMENT + 16 * BABYKBER_ADDR_OFFSETT, demo_ct[0]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 196 * BABYKBER_ADDR_INCREMENT + 17 * BABYKBER_ADDR_OFFSETT, demo_ct[1]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 200 * BABYKBER_ADDR_INCREMENT + 17 * BABYKBER_ADDR_OFFSETT, demo_ct[2]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 208 * BABYKBER_ADDR_INCREMENT + 17 * BABYKBER_ADDR_OFFSETT, demo_ct[3]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 209 * BABYKBER_ADDR_INCREMENT + 17 * BABYKBER_ADDR_OFFSETT, demo_ct[4]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 210 * BABYKBER_ADDR_INCREMENT + 17 * BABYKBER_ADDR_OFFSETT, demo_ct[5]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 212 * BABYKBER_ADDR_INCREMENT + 18 * BABYKBER_ADDR_OFFSETT, demo_ct[6]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 216 * BABYKBER_ADDR_INCREMENT + 18 * BABYKBER_ADDR_OFFSETT, demo_ct[7]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 224 * BABYKBER_ADDR_INCREMENT + 18 * BABYKBER_ADDR_OFFSETT, demo_ct[8]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 225 * BABYKBER_ADDR_INCREMENT + 18 * BABYKBER_ADDR_OFFSETT, demo_ct[9]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 226 * BABYKBER_ADDR_INCREMENT + 18 * BABYKBER_ADDR_OFFSETT, demo_ct[10]);
+    babykyber_write_reg(BABYKBER_BASE_ADDR + 228 * BABYKBER_ADDR_INCREMENT + 19 * BABYKBER_ADDR_OFFSETT, demo_ct[11]);
     
     // Trigger decryption
     babykyber_write_reg(BABYKYBER_DECRYPT_TRIGGER, 0);
--- src/babykyber_hal.c
+++ src/babykyber_hal.c
@@ -1,31 +1,23 @@
-#ifndef BABYKYBER_HAL_C
-#define BABYKYBER_HAL_C
-
 #include "babykyber_hal.h"
-#include <stdint.h>
 
-// Write a 32-bit value to a register
+// Hardware Abstraction Layer implementation
+
 void babykyber_write_reg(uint32_t addr, uint32_t value) {
     *(volatile uint32_t *)addr = value;
 }
 
-// Read a 32-bit value from a register
 uint32_t babykyber_read_reg(uint32_t addr) {
     return *(volatile uint32_t *)addr;
 }
 
-// Write an array of 32-bit values to consecutive registers
 void babykyber_write_array(uint32_t base_addr, const uint32_t *data, uint32_t count) {
-    for (uint32_t i = 0; i < count; ++i) {
+    for (uint32_t i = 0; i < count; i++) {
         babykyber_write_reg(base_addr + i * 4, data[i]);
     }
 }
 
-// Read an array of 32-bit values from consecutive registers
 void babykyber_read_array(uint32_t base_addr, uint32_t *data, uint32_t count) {
-    for (uint32_t i = 0; i < count; ++i) {
+    for (uint32_t i = 0; i < count; i++) {
         data[i] = babykyber_read_reg(base_addr + i * 4);
     }
 }
-
-#endif // BABYKYBER_HAL_C
